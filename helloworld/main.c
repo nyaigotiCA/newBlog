dea10008e9ab402869d2c80bd5386970969cc09d
nyaigotiCA <158310883+nyaigotiCA@users.noreply.github.com>
2024-01-31 19:19:33 +0300

    Add files via upload
    New programming files

---
+++ helloworld/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("MY NAME IS CLINTON NYAIGOTI!\n");
+     printf("MY COURSE IS CIVIL ENGINEERING!\n");
+      printf("MY CLUB IS NEWCASTLE!\n");
+       printf("MY FAVOURITE FOOD IS NYAMA!\n");
+    return 0;
+}
